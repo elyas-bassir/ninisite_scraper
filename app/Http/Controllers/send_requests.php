e16f614ed20afc7880e41769973f213843159356
elyas bassir <75251142+elyas-bassir@users.noreply.github.com>
2024-02-04 17:46:24 +0330

    update

--- app/Http/Controllers/send_requests.php
+++ app/Http/Controllers/send_requests.php
@@ -30,6 +30,13 @@ class send_requests extends Controller
         $body = find_element::load($response->body());
         $question_topic = $body->findOne(".topic-title")->text();
         $question = $body->findOne('.post-toggle > div:nth-child(1)')->text();
+        $img = $body->findOne('.post-toggle > div:nth-child(1)')->find('img');
+        $image_link=[];
+        if($img != null){
+            foreach($img as $key=>$value){
+                array_push($image_link,$value->getAttribute('src'));
+            }
+        }
         $name = explode(" ", delete_space($body->findOne('#topic > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)')->text()));
         $array_question += ['data_question' => ['person' => [
             'name' => $name[0],
@@ -37,12 +44,23 @@ class send_requests extends Controller
             'role' => $name[2],
             'register'=>$name[3].$name[4],
             'post'=>$name[5].$name[6].$name[7]
-        ], 'question_topic' => $question_topic, 'question' => $question]];
-
+        ],
+        'image_link'=>$image_link,
+        'question_topic' => $question_topic, 'question' => $question]];
+        $image_link_comment=[];
         $solutions = $body->findOne('#posts')->find('article');
         foreach($solutions as $key=>$val){
+            $image_link_comment=[];
+            $img = $val->find('img');
+            if($img != null){
+                foreach($img as $key=>$value){
+                    if($value->getAttribute('src')!="https://s1.ninifile.com/statics/default/blank-loading.png?width=80&amp;height=85&amp;crop&amp;bgcolor=white"){
+                        array_push($image_link_comment,$value->getAttribute('src'));
+                    }
+                }
+            }
             $name_commented = explode(" ", delete_space($val->findOne("div:nth-child(1) > div:nth-child(1)")->text()));
-            array_push($array_question,['name' => $name_commented[0],'comment'=>$val->findOne('.post-message')->text()]);
+            array_push($array_question,['name' => $name_commented[0],'comment'=>$val->findOne('.post-message')->text(),'image_link'=>$image_link_comment]);
         }
 
         return response()->json($array_question,200);
